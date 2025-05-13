5ab3262e16c5ed0e4df45c32aa7425d267e42853
augustojbe <augustojbe>
2025-05-13 01:12:13 -0300

    implantacao do form de cadastro e estilizacao

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import car_viw
+from cars.views import car_viw, new_car_viw
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', car_viw, name='cars_list'),
+    path('new_car/', new_car_viw, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
+from cars.forms import CarForm
 
 def car_viw(request):
     cars = Car.objects.all().order_by('model')
@@ -12,3 +13,13 @@ def car_viw(request):
     return render(request,
                   'cars.html',
                   {'cars': cars})
+    
+def new_car_viw(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarForm()
+    return render(request, 'new_car.html', {'new_car_form': new_car_form} )
