46376a154b307a985c3265402954afd86632a5fc
augustojbe <augustojbe>
2025-05-13 12:51:07 -0300

    implantacao do sistema de login e criacao de rotas especificas

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import car_viw, new_car_viw
+from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('cars/', car_viw, name='cars_list'),
     path('new_car/', new_car_viw, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms import CarModelForm
 
 def car_viw(request):
     cars = Car.objects.all().order_by('model')
@@ -16,10 +16,10 @@ def car_viw(request):
     
 def new_car_viw(request):
     if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES)
+        new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
     else:
-        new_car_form = CarForm()
+        new_car_form = CarModelForm()
     return render(request, 'new_car.html', {'new_car_form': new_car_form} )
